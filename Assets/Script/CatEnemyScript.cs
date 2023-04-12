1dd4ddc2012e38cfe324947db827934399913c50
uraiblakhani2 <82686794+uraiblakhani2@users.noreply.github.com>
2023-04-12 00:18:28 -0400

    lab9
    lab9

--- Assets/Script/CatEnemyScript.cs
+++ Assets/Script/CatEnemyScript.cs
@@ -1,32 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.AI;
 
 public class CatEnemyScript : MonoBehaviour
 {
-       public GameObject player;
+    public GameObject player;
+    public GameObject[] patrolPoints;
+    private int currentPatrolPointIndex;
+    private NavMeshAgent navMeshAgent;
 
     public float maxAngle = 45;
     public float maxDistance = 2;
-    public float timer = 1.0f;
-    public float visionCheckRate = 1.0f;
-    // Start is called before the first frame update
+    public float patrolSpeed = 3.0f;
+    public float chaseSpeed = 6.0f;
+    public float stoppingDistance = 0.5f;
+
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        currentPatrolPointIndex = 0;
+        navMeshAgent.speed = patrolSpeed;
+        navMeshAgent.stoppingDistance = stoppingDistance;
+        GoToNextPatrolPoint();
+    }
+
+    void Update()
+    {
+        if (SeePlayer())
+        {
+            navMeshAgent.speed = chaseSpeed;
+            navMeshAgent.destination = player.transform.position;
+        }
+        else
+        {
+            navMeshAgent.speed = patrolSpeed;
+            if (!navMeshAgent.pathPending && navMeshAgent.remainingDistance < stoppingDistance)
+            {
+                GoToNextPatrolPoint();
+            }
+        }
+    }
+
+    void OnCollisionEnter(Collision c)
+    {
+        if (c.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
     }
 
 
-    // Update is called once per frame
-    void Update()
-    { 
-        if(SeePlayer())
-        {
-            // Go to Player
-        }
-        else {
-            // Patrol
-        }
+
+    private void GoToNextPatrolPoint()
+    {
+        if (patrolPoints.Length == 0) return;
+
+        navMeshAgent.destination = patrolPoints[currentPatrolPointIndex].transform.position;
+        currentPatrolPointIndex = (currentPatrolPointIndex + 1) % patrolPoints.Length;
     }
 
     public bool SeePlayer()
@@ -36,25 +69,27 @@ public class CatEnemyScript : MonoBehaviour
         {
             return false;
         }
+
+        Quaternion targetRotation = Quaternion.LookRotation(vecPlayerTurret);
+        transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, Time.deltaTime);
+
         Vector3 normVecPlayerTurret = Vector3.Normalize(vecPlayerTurret);
-        float dotProduct = Vector3.Dot(transform.forward,normVecPlayerTurret);
+        float dotProduct = Vector3.Dot(transform.forward, normVecPlayerTurret);
         var angle = Mathf.Acos(dotProduct);
         float deg = angle * Mathf.Rad2Deg;
         if (deg < maxAngle)
         {
             RaycastHit hit;
-            Ray ray = new Ray(transform.position,transform.forward);
-        
+            Ray ray = new Ray(transform.position, vecPlayerTurret);
+
             if (Physics.Raycast(ray, out hit))
             {
                 if (hit.collider.tag == "Player")
                 {
                     return true;
                 }
-                
             }
         }
         return false;
-       
     }
 }
--- Assets/Script/MovePlayer.cs
+++ Assets/Script/MovePlayer.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
+    public float speed = 5.0f;
+    public float rotationSpeed = 200.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(0, 0, vertical);
+        moveDirection = transform.TransformDirection(moveDirection);
+        moveDirection *= speed;
+
+        transform.position += moveDirection * Time.deltaTime;
+        transform.Rotate(0, horizontal * rotationSpeed * Time.deltaTime, 0);
     }
 }
